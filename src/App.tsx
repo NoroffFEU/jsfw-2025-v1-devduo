99426fa60de53fafb1e1fcd2533007137700477c
HelChris <111705707+HelChris@users.noreply.github.com>
2026-02-23 15:16:00 +0100

    hello world

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+
+
+  return (
+    <>
+      <h1>Hello World</h1>
+      <p>Welcome, Human. The page you are looking for is under construction.</p>
+      <small>WevDuo's online shop coming soon</small>
+    </>
+  );
+}
+
+export default App;
