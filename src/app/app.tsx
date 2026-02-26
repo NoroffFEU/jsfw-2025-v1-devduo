fdb09e0c33bcfd969ca821137a079bbde7380f55
HelChris <111705707+HelChris@users.noreply.github.com>
2026-02-26 18:47:59 +0100

    add react-router-dom in app.tsx

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,10 +1,26 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Layout from '../components/layout/layout.tsx';
+import CartPage from '../pages/cartPage.tsx';
+import CheckoutSuccessPage from '../pages/checkoutSuccessPage.tsx';
+import ContactPage from '../pages/contactPage.tsx';
+import HomePage from '../pages/homePage.tsx';
+import PageNotFoundPage from '../pages/pageNotFoundPage.tsx';
+import ProductDetailPage from '../pages/productDetailPage.tsx';
+
 function App() {
   return (
-    <main>
-      <h1 className="color-text">Hello World</h1>
-      <p>Welcome, Human. The page you are looking for is under construction.</p>
-      <small>WevDuo's online shop coming soon</small>
-    </main>
+    <BrowserRouter>
+      <Layout>
+        <Routes>
+          <Route path="" element={<HomePage />} />
+          <Route path="/cart" element={<CartPage />} />
+          <Route path="/checkout" element={<CheckoutSuccessPage />} />
+          <Route path="/contact" element={<ContactPage />} />
+          <Route path="/notfound" element={<PageNotFoundPage />} />
+          <Route path="/product/:id" element={<ProductDetailPage />} />
+        </Routes>
+      </Layout>
+    </BrowserRouter>
   );
 }
 
