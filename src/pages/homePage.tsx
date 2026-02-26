1fd5d97cb433dc41636c7cb4e92c5b498eef129d
HelChris <111705707+HelChris@users.noreply.github.com>
2026-02-26 18:43:47 +0100

    add pages

---
+++ src/pages/homePage.tsx
@@ -0,0 +1,11 @@
+const HomePage = () => {
+  return (
+    <main>
+      <h1 className="color-text">Hello World</h1>
+      <p>Welcome, Human. The page you are looking for is under construction.</p>
+      <small>WevDuo's online shop coming soon</small>
+  </main>
+  );
+};
+
+export default HomePage;
