0617d97b3838e3e83a907337406a0f2087c96d8b
Tore Hirth <torehirth@gmail.com>
2026-03-04 22:47:33 +0100

    delete script for testing:ui

--- src/testing/setupTests.ts
+++ src/testing/setupTests.ts
@@ -2,6 +2,7 @@ import { cleanup } from "@testing-library/react";
 import { afterEach, vi } from "vitest";
 import "@testing-library/jest-dom";
 
+// This runs after each test, ensuring that the DOM is cleaned up and all mocks are cleared to prevent test interference.
 afterEach(() => {
   cleanup();
   vi.clearAllMocks();
