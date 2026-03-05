82a641948b4a825c1ff843b0d24a9edfd7f5a1b0
Tore Hirth <torehirth@gmail.com>
2026-03-05 12:40:30 +0100

    renamed tsx files in /pages to start with capital letter

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -4,7 +4,7 @@ const HomePage = () => {
       <h1 className="color-text">Hello World</h1>
       <p>Welcome, Human. The page you are looking for is under construction.</p>
       <small>WevDuo's online shop coming soon</small>
-  </main>
+    </main>
   );
 };
 
