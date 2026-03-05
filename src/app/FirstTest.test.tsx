74ac0c1b85cb0c7572ef06e5a008c1d0d03e79f8
Tore Hirth <torehirth@gmail.com>
2026-03-05 11:00:47 +0100

    format code

--- src/app/FirstTest.test.tsx
+++ src/app/FirstTest.test.tsx
@@ -6,7 +6,6 @@ describe("First component", () => {
   it("Should render the heading", () => {
     const { getByRole } = render(<FirstTest />);
     const heading = getByRole("heading");
-
     expect(heading).toBeInTheDocument();
   });
 });
