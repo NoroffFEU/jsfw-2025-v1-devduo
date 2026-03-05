0807bbf5d7245096b669282629b41d9113c3d83c
Tore Hirth <torehirth@gmail.com>
2026-03-05 23:25:12 +0100

    update typography css

--- src/components/layout/header/Header.tsx
+++ src/components/layout/header/Header.tsx
@@ -1,7 +1,12 @@
 import styles from "./Header.module.css";
 
 const Header = () => {
-  return <header className={styles.headerContainer}>Header is coming here</header>;
+  return (
+    <header className={styles.headerContainer}>
+      Header is coming here
+      <a href="asd">test</a>
+    </header>
+  );
 };
 
 export default Header;
