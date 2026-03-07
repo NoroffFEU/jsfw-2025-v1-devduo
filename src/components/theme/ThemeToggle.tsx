7afa57c6efaa17823341c6695dc4e7a7dff4996f
HelChris <111705707+HelChris@users.noreply.github.com>
2026-03-07 12:02:13 +0100

    clear themetoggle files and utils

--- src/components/theme/ThemeToggle.tsx
+++ src/components/theme/ThemeToggle.tsx
@@ -1,16 +1,1 @@
-type Theme = "light" | "dark";
-
-function ThemeToggle() {
-  function handleToggle(theme: Theme) {
-    document.documentElement.setAttribute("data-theme", theme);
-  }
-
-  return (
-    <div role="group" aria-label="Theme toggle">
-      <button onClick={() => handleToggle("light")}>Light</button>
-      <button onClick={() => handleToggle("dark")}>Dark</button>
-    </div>
-  );
-}
-
-export default ThemeToggle;
+/* ThemeToggle whoho*/
