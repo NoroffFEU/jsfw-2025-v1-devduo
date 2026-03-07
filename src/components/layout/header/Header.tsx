dbc86e42fb081e30789c904ba7f834b2c965ce75
Tore Hirth <torehirth@gmail.com>
2026-03-07 16:28:04 +0100

    remove mobile menu icon button and close button

--- src/components/layout/header/Header.tsx
+++ src/components/layout/header/Header.tsx
@@ -2,11 +2,13 @@ import styles from "./Header.module.css";
 import Logo from "./Logo";
 import NavLinks from "./NavLinks";
 import CartLink from "./CartLink";
+import HamburgerMenu from "./HamburgerMenu";
 
 const Header = () => {
   return (
     <header className={styles.headerContainer}>
       <div className={styles.headerWrapper}>
+        <HamburgerMenu />
         <Logo />
         <NavLinks />
         <CartLink />
