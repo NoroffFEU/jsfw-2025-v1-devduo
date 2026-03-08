03e1c3f3c2a4fd84bee2c4df479f485ff07cafa4
Tore Hirth <torehirth@gmail.com>
2026-03-08 15:51:33 +0100

    feat: highlight active navigation link using React Router

--- src/components/layout/header/NavLinks.tsx
+++ src/components/layout/header/NavLinks.tsx
@@ -9,16 +9,23 @@ type ClassNameProps = {
   className?: string;
 };
 
+// React Router passes an object with the property isActive (boolean).
+// Did not destructure the prop (which is normal) to understand better whats going on in the helper function.
+// If isActive is false only the link class gets implemented, and both if it's true.
+const setLinkStyle = (props: { isActive: boolean }) => {
+  return props.isActive ? `${styles.link} ${styles.activeLink}` : styles.link;
+};
+
 const NavLinks = ({ className }: ClassNameProps) => {
   return (
     <nav className={`${styles.linksContainer} ${className}`}>
-      <NavLink to="/" className={styles.link}>
+      <NavLink to="/" className={setLinkStyle} end>
         Home
       </NavLink>
-      <NavLink to="/products" className={styles.link}>
+      <NavLink to="/products" className={setLinkStyle}>
         Products
       </NavLink>
-      <NavLink to="/contact" className={styles.link}>
+      <NavLink to="/contact" className={setLinkStyle}>
         Contact
       </NavLink>
     </nav>
