4b411f457511d7477f7470aa71bd68ad2169dcf4
Tore Hirth <torehirth@gmail.com>
2026-03-10 00:00:56 +0100

    feat: add product card component 1 step

---
+++ src/features/products/components/ProductCard.tsx
@@ -0,0 +1,13 @@
+// import { Link } from "react-router-dom"
+// import productImage from "../../../assets/images/test-image.webp"
+
+// const ProductCard = () => {
+//   return (
+//     <Link>
+//     <div>
+//       <img src={productImage} alt="Example product" />
+//       <p></p>
+//     </div>
+//     </Link>
+//   )
+// }
