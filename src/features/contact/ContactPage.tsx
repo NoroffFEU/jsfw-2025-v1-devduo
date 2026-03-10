2845f69e607cf21f72f715e4208029c56da8c781
HelChris <111705707+HelChris@users.noreply.github.com>
2026-03-10 23:22:07 +0100

    add: responsive draft for contact page

--- src/features/contact/ContactPage.tsx
+++ src/features/contact/ContactPage.tsx
@@ -1,5 +1,32 @@
+import { ContactForm } from "./components/ContactComponent.tsx";
+import styles from "./components/ContactComponent.module.css";
+
 const ContactPage = () => {
-  return <main>Contact Page</main>;
+  return (
+    <div className={styles.contactPageWrapper}>
+      <div className={styles.contactText}>
+        <h1>Contact Us</h1>
+        <p>
+          Have a question or feedback? <br /> We'd love to hear from you.
+        </p>
+      </div>
+      <ContactForm />
+      <section className={styles.contactCardsContainer}>
+        <article className={styles.contactCards}>
+          <h2>Response</h2>
+          <p>Quick Response</p>
+        </article>
+        <article className={styles.contactCards}>
+          <h2>Email</h2>
+          <p>support@webstore.com</p>
+        </article>
+        <article className={styles.contactCards}>
+          <h2>Support</h2>
+          <p>We're here to help</p>
+        </article>
+      </section>
+    </div>
+  );
 };
 
 export default ContactPage;
--- src/features/contact/components/ContactComponent.tsx
+++ src/features/contact/components/ContactComponent.tsx
@@ -1,1 +1,48 @@
-// Some contact page component
+import styles from "./ContactComponent.module.css";
+import { UserIcon } from "@heroicons/react/24/outline";
+import { EnvelopeIcon } from "@heroicons/react/24/outline";
+import { ChatBubbleLeftIcon } from "@heroicons/react/24/outline";
+import { PaperAirplaneIcon } from "@heroicons/react/24/outline";
+
+export const ContactForm = () => {
+  return (
+    <form className={styles.contactFormWrapper}>
+      <div>
+        <div className={styles.contactFormDiv}>
+          <label className={styles.contactLabel}>
+            <UserIcon className={styles.iconForm} />
+            Full Name*
+          </label>
+          <input className={styles.contactInput} />
+        </div>
+      </div>
+      <div>
+        <div className={styles.contactFormDiv}>
+          <label className={styles.contactLabel}>
+            <EnvelopeIcon className={styles.iconForm} />
+            Email*
+          </label>
+          <input className={styles.contactInput} />
+        </div>
+      </div>
+      <div>
+        <div className={styles.contactFormDiv}>
+          <label className={styles.contactLabel}>
+            <ChatBubbleLeftIcon className={styles.iconForm} />
+            Subject*
+          </label>
+          <input className={styles.contactInput} />
+        </div>
+      </div>
+      <div>
+        <div className={styles.contactFormDiv}>
+          <label className={styles.contactLabel}>
+            <PaperAirplaneIcon className={styles.iconForm} />
+            Message*
+          </label>
+          <input className={styles.contactInput} />
+        </div>
+      </div>
+    </form>
+  );
+};
