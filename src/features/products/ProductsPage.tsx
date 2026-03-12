450bdf2865a4c9a8738a6d489421c7d7817b988f
Tore Hirth <torehirth@gmail.com>
2026-03-12 20:48:46 +0100

    feature: create product card. add singel card to product page

--- src/features/products/ProductsPage.tsx
+++ src/features/products/ProductsPage.tsx
@@ -1,7 +1,15 @@
+import ProductCard from "./components/ProductCard";
+import styles from "./ProductsPage.module.css";
+
 const ProductsPage = () => {
   return (
     <>
-      <h1>All Products</h1>
+      <h1 className={styles.productHeadline}>Our Products</h1>
+      <section>
+        {/* Sort by categories here */}
+        {/* Grid for all products here */}
+        <ProductCard />
+      </section>
     </>
   );
 };
--- src/features/products/components/ProductCard.tsx
+++ src/features/products/components/ProductCard.tsx
@@ -1,13 +1,48 @@
-// import { Link } from "react-router-dom"
-// import productImage from "../../../assets/images/test-image.webp"
+import { Link } from "react-router-dom";
+import productImage from "../../../assets/images/test-image.webp";
+import placeholderImage from "../../../assets/images/placeholder.webp";
+import styles from "./ProductCard.module.css";
+import { StarIcon } from "@heroicons/react/24/solid";
+import DiscountBadge from "./DiscountBadge";
+import { calculateDiscount } from "../utils/calculateDiscount";
 
-// const ProductCard = () => {
-//   return (
-//     <Link>
-//     <div>
-//       <img src={productImage} alt="Example product" />
-//       <p></p>
-//     </div>
-//     </Link>
-//   )
-// }
+// THESE CONSTANTS/VALUES WILL GET REPLACED WITH API DATA.
+// DELETE BEFORE DEPLOYMENT
+const productName: string = "Awesome title";
+const discountedPrice: number = 450;
+const defaultPrice: number = 499;
+const rating: number = 4.5;
+const imageAltText = "";
+// ------------------------------------
+
+const discountValue = calculateDiscount({ defaultPrice, discountedPrice });
+
+const ProductCard = () => {
+  return (
+    <Link to="../ProductDetailPage" className={styles.productLink}>
+      <article className={styles.productCardContainer}>
+        <img
+          className={styles.productImage}
+          src={productImage ? productImage : placeholderImage}
+          alt={imageAltText ? imageAltText : "Product image"}
+        />
+        {discountValue ? <DiscountBadge discount={discountValue} /> : ""}
+        <div className={styles.infoContainer}>
+          <h3>{productName}</h3>
+          <p className={styles.ratingElement}>
+            <span className={styles.starIcon}>
+              <StarIcon />
+            </span>
+            {rating}
+          </p>
+          <div className={styles.priceContainer}>
+            <p>${discountedPrice}</p>
+            <p>${defaultPrice}</p>
+          </div>
+        </div>
+      </article>
+    </Link>
+  );
+};
+
+export default ProductCard;
