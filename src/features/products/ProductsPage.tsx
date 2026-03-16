92942ca9cdb53ac95d65df51b5ac0e6476cb0649
Tore Hirth <torehirth@gmail.com>
2026-03-16 15:17:17 +0100

    refactor: update the these components to control the API response in other components

--- src/features/products/ProductsPage.tsx
+++ src/features/products/ProductsPage.tsx
@@ -1,15 +1,42 @@
+import { useEffect, useState } from "react";
 import ProductsGrid from "./components/ProductsGrid";
 import styles from "./ProductsPage.module.css";
+import type { APIResponse } from "../../types/types";
+import { fetchProducts } from "./services/fetchProducts";
 
 const ProductsPage = () => {
+  const [products, setProducts] = useState<APIResponse | null>(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    const loadProducts = async () => {
+      try {
+        setLoading(true);
+        const data = await fetchProducts();
+        setProducts(data);
+      } catch (err) {
+        setError(
+          err instanceof Error
+            ? `${err.message}. Try again later`
+            : `Something went wrong. Try again later`,
+        );
+
+        console.error(err, "Fetch request failed");
+      } finally {
+        setLoading(false);
+      }
+    };
+    loadProducts();
+  }, []);
+
+  const productsList = products?.data;
   return (
     <>
       <section>
         <h1 className={styles.productHeadline}>Our Products</h1>
-        {/* Sort by categories here */}
-        {/* Grid for all products here */}
       </section>
-      <ProductsGrid />
+      <ProductsGrid error={error} loading={loading} productsList={productsList} />
     </>
   );
 };
--- src/features/products/components/ProductsGrid.tsx
+++ src/features/products/components/ProductsGrid.tsx
@@ -1,44 +1,18 @@
-import { useEffect, useState } from "react";
 import ProductCard from "./ProductCard";
 import styles from "./ProductsGrid.module.css";
-import { fetchProducts } from "../services/fetchProducts";
 import ErrorMessage from "../../../components/shared/error/ErrorMessage";
 import Loader from "../../../components/shared/loader/Loader";
-import type { APIResponse } from "../../../types/types";
+import type { ProductGridProps } from "../../../types/types";
 
-const ProductsGrid = () => {
-  const [products, setProducts] = useState<APIResponse | null>(null);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState<string | null>(null);
-
-  useEffect(() => {
-    const loadProducts = async () => {
-      try {
-        setLoading(true);
-        const data = await fetchProducts();
-        setProducts(data);
-      } catch (err) {
-        setError(
-          err instanceof Error
-            ? `${err.message}. Try again later`
-            : `Something went wrong. Try again later`,
-        );
-
-        console.error(err, "Fetch request failed");
-      } finally {
-        setLoading(false);
-      }
-    };
-    loadProducts();
-  }, []);
-
-  const itemsArray = products?.data;
+const ProductsGrid = ({ error, loading, productsList }: ProductGridProps) => {
+  // Only display error and loader if they are true
+  // These statements prevents both from being displayed at the same time, rather than having them inside the section element.
+  if (error) return <ErrorMessage message={error} />;
+  if (loading) return <Loader />;
 
   return (
     <section className={styles.productsGridContainer}>
-      {error && <ErrorMessage message={error} />}
-      {loading && <Loader />}
-      {itemsArray?.map((product) => (
+      {productsList?.map((product) => (
         <ProductCard
           key={product.id}
           productId={product.id}
