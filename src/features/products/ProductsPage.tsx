81bf9f708f066fe8db5112d177c0d7aa27028726
Tore Hirth <torehirth@gmail.com>
2026-03-16 12:18:35 +0100

    refactor: move the products grid and fetching to a new component

--- src/features/products/ProductsPage.tsx
+++ src/features/products/ProductsPage.tsx
@@ -1,39 +1,7 @@
-import type { APIResponse } from "../../types/types";
-import ProductCard from "./components/ProductCard";
+import ProductsGrid from "./components/ProductsGrid";
 import styles from "./ProductsPage.module.css";
-import { useEffect, useState } from "react";
-import { fetchProducts } from "./services/fetchProducts";
-import ErrorMessage from "../../components/shared/error/ErrorMessage";
-import Loader from "../../components/shared/loader/Loader";
 
 const ProductsPage = () => {
-  const [products, setProducts] = useState<APIResponse | null>(null);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState<string | null>(null);
-
-  useEffect(() => {
-    const loadProducts = async () => {
-      try {
-        setLoading(true);
-        const data = await fetchProducts();
-        setProducts(data);
-      } catch (err) {
-        setError(
-          err instanceof Error
-            ? `${err.message}. Try again later`
-            : `Something went wrong. Try again later`,
-        );
-
-        console.error(err, "Fetch request failed");
-      } finally {
-        setLoading(false);
-      }
-    };
-    loadProducts();
-  }, []);
-
-  const itemsArray = products?.data;
-
   return (
     <>
       <section>
@@ -41,22 +9,7 @@ const ProductsPage = () => {
         {/* Sort by categories here */}
         {/* Grid for all products here */}
       </section>
-      <section className={styles.productsGridContainer}>
-        {error && <ErrorMessage message={error} />}
-        {loading && <Loader />}
-        {itemsArray?.map((product) => (
-          <ProductCard
-            key={product.id}
-            productId={product.id}
-            productName={product.title}
-            productImage={product.image?.url}
-            imageAltText={product.image?.alt}
-            defaultPrice={product.price}
-            discountedPrice={product.discountedPrice}
-            rating={product?.rating}
-          />
-        ))}
-      </section>
+      <ProductsGrid />
     </>
   );
 };
