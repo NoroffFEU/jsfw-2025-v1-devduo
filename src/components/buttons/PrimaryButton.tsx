35b5a47fb538c1568142b37211223cd89d00f90e
Tore Hirth <torehirth@gmail.com>
2026-03-17 15:02:06 +0100

    feat: create reusable primary button component

--- src/components/buttons/PrimaryButton.tsx
+++ src/components/buttons/PrimaryButton.tsx
@@ -1,1 +1,28 @@
-// Primary button component
+import { Link } from "react-router-dom";
+import styles from "./PrimaryButton.module.css";
+
+type PrimaryButtonProps = {
+  path: string;
+  text: string;
+};
+
+/**
+ * A primary button component that navigates to a specified path.
+ *
+ * @component
+ * @example
+ * const Example = () => (
+ *   <PrimaryButton path="/dashboard" text="Go to Dashboard" />
+ * );
+ * @returns {JSX.Element} A styled link element functioning as a primary button
+ */
+
+const PrimaryButton = ({ path, text }: PrimaryButtonProps) => {
+  return (
+    <Link to={path} className={styles.PrimaryButton}>
+      {text}
+    </Link>
+  );
+};
+
+export default PrimaryButton;
