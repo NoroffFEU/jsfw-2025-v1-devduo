6f6f581b85954b4252359d77c1cb8e6ac9c16113
Tore Hirth <torehirth@gmail.com>
2026-03-17 15:26:36 +0100

    refactor: delete empty css-file. refactor the homepage component

--- src/features/home/HomePage.tsx
+++ src/features/home/HomePage.tsx
@@ -1,55 +1,13 @@
-import { useEffect, useState } from "react";
-import ProductsGrid from "../products/components/ProductsGrid";
-import styles from "./HomePage.module.css";
-import { fetchProducts } from "../products/services/fetchProducts";
-import type { APIResponse } from "./../../types/types";
+import ExploreSection from "./components/ExploreSection";
+import HeroSection from "./components/HeroSection";
+import NewProductsSection from "./components/NewProductsSection";
 
 const HomePage = () => {
-  const [products, setProducts] = useState<APIResponse | null>(null);
-  const [loading, setLoading] = useState<boolean>(true);
-  const [error, setError] = useState<string | null>(null);
-
-  useEffect(() => {
-    const loadProducts = async () => {
-      try {
-        setLoading(true);
-        const data = await fetchProducts();
-        setProducts(data);
-      } catch (err) {
-        setError(
-          err instanceof Error
-            ? `${err.message}. Try again later`
-            : `Something went wrong. Try again later`,
-        );
-        console.error(err, "Fetch request failed");
-      } finally {
-        setLoading(false);
-      }
-    };
-    loadProducts();
-  }, []);
-
-  // Only display the first 4 products
-  const productsList = products?.data.slice(0, 4);
-
   return (
     <>
-      <section className={styles.heroWrapper}>
-        <h1 className="color-text">Welcome to our store</h1>
-        <p>
-          Discover amazing products at great prices. Quality and style delivered to your
-          door.
-        </p>
-        {/* Search bar is displayed here */}
-      </section>
-      <section className={styles.productOuterContainer}>
-        <ProductsGrid
-          error={error}
-          loading={loading}
-          productsList={productsList}
-          className={styles.productWrapper}
-        />
-      </section>
+      <HeroSection />
+      <NewProductsSection />
+      <ExploreSection />
     </>
   );
 };
