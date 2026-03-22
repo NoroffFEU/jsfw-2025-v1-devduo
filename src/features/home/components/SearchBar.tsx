78c6ee6abd73bdbcb3a3188817e5eee544af3663
Tore Hirth <torehirth@gmail.com>
2026-03-22 21:20:56 +0100

    refactor: update search bar component with minor upgrades to ui/ux

--- src/features/home/components/SearchBar.tsx
+++ src/features/home/components/SearchBar.tsx
@@ -1,19 +1,20 @@
 import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
 import styles from "./SearchBar.module.css";
-import ProductsGrid from "./../../products/components/ProductsGrid";
 import { useEffect, useState } from "react";
 import { fetchProducts } from "../../products/services/fetchProducts";
 import type { APIResponse } from "../../../types/types";
+import ProductsGrid from "../../products/components/ProductsGrid";
 
 const SearchBar = () => {
   const [loading, setLoading] = useState<boolean>(true);
-  const [error, setError] = useState<string | null>("");
+  const [error, setError] = useState<string | null>(null);
   const [products, setProducts] = useState<APIResponse | null>(null);
   const [query, setQuery] = useState<string>("");
 
   useEffect(() => {
     const loadProducts = async () => {
       try {
+        setError(null);
         setLoading(true);
         const data = await fetchProducts();
         setProducts(data);
@@ -26,7 +27,6 @@ const SearchBar = () => {
     };
     loadProducts();
   }, []);
-  console.log(products?.data);
 
   const filteredProducts = products?.data.filter((items) => {
     return items?.title.toLowerCase().includes(query.toLowerCase());
@@ -43,12 +43,18 @@ const SearchBar = () => {
           placeholder="Search..."
           className={styles.searchBar}
           onChange={(event) => setQuery(event.target.value)}
+          aria-label="Search products"
         />
-        <label aria-hidden="true">
+        <span aria-hidden="true">
           <MagnifyingGlassIcon className={styles.searchIcon} />
-        </label>
+        </span>
       </div>
       <div>
+        {filteredProducts && filteredProducts.length === 0 && (
+          <p className={styles.message}>
+            {`No products found on the search query: ${query}`}
+          </p>
+        )}
         {query && filteredProducts && filteredProducts.length > 0 && (
           <ProductsGrid
             loading={loading}
