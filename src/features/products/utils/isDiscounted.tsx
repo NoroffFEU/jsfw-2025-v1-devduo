bdc7089a57b071dbe09b1870d4e0567f6fb293d9
HelChris <111705707+HelChris@users.noreply.github.com>
2026-03-24 18:22:21 +0100

    add: returns false instead of null

--- src/features/products/utils/isDiscounted.tsx
+++ src/features/products/utils/isDiscounted.tsx
@@ -3,7 +3,7 @@ import type { DiscountPriceType } from "../../../types/types";
 // returns true if discounted price is lesser than default price
 export const isDiscounted = ({ defaultPrice, discountedPrice }: DiscountPriceType) => {
   if (discountedPrice === undefined) {
-    return null;
+    return false;
   }
   return discountedPrice < defaultPrice;
 };
