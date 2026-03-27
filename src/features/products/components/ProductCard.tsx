e2b536750dc1339bd21f5abd273079ed10381918
HelChris <111705707+HelChris@users.noreply.github.com>
2026-03-27 15:55:56 +0100

    refactor: productdetail fetched with id and displayed by slug with react use hook suspense as fallback

--- src/features/products/components/ProductCard.tsx
+++ src/features/products/components/ProductCard.tsx
@@ -1,14 +1,15 @@
 import { Link } from "react-router-dom";
+import { slugify } from "../utils/slugify";
 import placeholderImage from "../../../assets/images/placeholder.webp";
 import styles from "./ProductCard.module.css";
 import { StarIcon } from "@heroicons/react/24/solid";
 import DiscountBadge from "./DiscountBadge";
 import { calculateDiscount } from "../utils/calculateDiscount";
 import { isDiscounted } from "../utils/isDiscounted";
-import { slugify } from "../utils/slugify";
 import type { ProductType } from "../../../types/types";
 
 const ProductCard = ({
+  productId,
   productName,
   productImage,
   imageAltText,
@@ -24,7 +25,10 @@ const ProductCard = ({
   const hasDiscount = isDiscounted({ defaultPrice, discountedPrice });
 
   return (
-    <Link to={`../product/${slug}`} className={styles.productLink}>
+    <Link
+      to={`../product/${slug}`}
+      state={{ id: productId }}
+      className={styles.productLink}>
       <article className={styles.productCardContainer}>
         <img
           className={styles.productImage}
