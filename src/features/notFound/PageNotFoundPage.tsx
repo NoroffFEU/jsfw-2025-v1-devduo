3e5a84e48a1a254a04a7f93445c27f451367a9a0
Tore Hirth <torehirth@gmail.com>
2026-03-28 17:49:55 +0100

    refactor: update to 10s and move message above button, add styles to stylesheet

--- src/features/notFound/PageNotFoundPage.tsx
+++ src/features/notFound/PageNotFoundPage.tsx
@@ -6,17 +6,17 @@ import { useEffect, useState } from "react";
 
 const PageNotFoundPage = () => {
   const navigate = useNavigate();
-  const [seconds, setSeconds] = useState<number>(8);
+  const [seconds, setSeconds] = useState<number>(10);
 
   useEffect(() => {
     // Countdown from 8s before navigating to homepage
     const countdown = setInterval(() => {
       setSeconds((prev) => prev - 1);
     }, 1000);
-    // Navigate after 8 seconds
+    // Navigate after 10 seconds
     const timer = setTimeout(() => {
       navigate("/");
-    }, 8000);
+    }, 10000);
 
     // cleanup
     return () => {
@@ -32,11 +32,11 @@ const PageNotFoundPage = () => {
           <ExclamationTriangleIcon className={styles.icon} aria-hidden="true" />
           <h1>Woops, something went wrong.</h1>
           <p>Page not found.</p>
+          <p className={styles.navigationParagraph}>
+            Navigates you automatically to homepage in {seconds} seconds
+          </p>
           <PrimaryButton path="/" text="Please return home" />
         </article>
-        <p style={{ textAlign: "center" }}>
-          Navigates you automatically to homepage in {seconds} seconds
-        </p>
       </section>
     </>
   );
