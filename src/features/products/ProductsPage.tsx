18ccd05c69aa0f1640687944486a0b533cc96299
Tore Hirth <torehirth@gmail.com>
2026-03-28 18:03:18 +0100

    refactor: add navigation to /notfound on HTTP-status 404

--- src/features/products/ProductsPage.tsx
+++ src/features/products/ProductsPage.tsx
@@ -3,11 +3,13 @@ import ProductsGrid from "./components/ProductsGrid";
 import styles from "./ProductsPage.module.css";
 import type { APIResponse } from "../../types/types";
 import { fetchProducts } from "./services/fetchProducts";
+import { useNavigate } from "react-router";
 
 const ProductsPage = () => {
   const [products, setProducts] = useState<APIResponse | null>(null);
   const [loading, setLoading] = useState<boolean>(true);
   const [error, setError] = useState<string | null>(null);
+  const navigate = useNavigate();
 
   useEffect(() => {
     const loadProducts = async () => {
@@ -16,12 +18,11 @@ const ProductsPage = () => {
         const data = await fetchProducts();
         setProducts(data);
       } catch (err) {
-        setError(
-          err instanceof Error
-            ? `${err.message}. Try again later`
-            : `Something went wrong. Try again later`,
-        );
-
+        // Redirects to notfound page if response.status === 404
+        if (err instanceof Error && err.message === "PAGE_NOT_FOUND") {
+          navigate("/notfound");
+        }
+        setError("Something went wrong. Try again later");
         console.error(err, "Fetch request failed");
       } finally {
         setLoading(false);
